5fba3618ae54086d09b41367c6fc2b7f88cb8c90
john davis <davis@doom.lan>
2024-11-19 13:14:52 +0700

    adding style to a stack and using tertiarty operator to style a button

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var useRedText = false
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Button("Toggle Red Text") {
+                useRedText.toggle()
+            }
+            .foregroundStyle(useRedText ? .red: .blue)
         }
+        .font(.largeTitle)
         .padding()
     }
 }
