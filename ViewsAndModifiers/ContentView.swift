5880307675f852a8db02d5500535d25a957094c2
john davis <davis@doom.lan>
2024-12-14 15:53:24 -0500

    A custom modifier

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,18 +7,15 @@
 
 import SwiftUI
 
-
-
-struct CapsuleText: View {
-    var text: String
-
-    var body: some View {
-        Text(text)
+// A custom modifier
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
             .font(.largeTitle)
+            .foregroundStyle(.primary)
             .padding()
-            .foregroundStyle(.white)
             .background(.blue)
-            .clipShape(.capsule)
+            .clipShape(.rect(cornerRadius: 10))
     }
 }
 
@@ -26,15 +23,14 @@ struct ContentView: View {
     
     
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("hello")
+                .modifier(Title())
+        } // vstack
         .padding()
-        .background(Color.blue)
-        .padding()
-        .background(Color.green)
-        .padding()
-        .background(Color.yellow)
-        .padding()
-        .background(Color.red)
     }
 }
 
